254e2d917ff1293f49596e256f462c401cf62548
faiza <faiza@mislbd.com>
2022-04-18 15:36:10 +0600

    fixes in transaction

--- gas-recharge-multiple.js
+++ gas-recharge-multiple.js
@@ -2,6 +2,54 @@ require("log-timestamp");
 const ethers = require("ethers");
 const lib = require('./crypto-lib');
 
+// async function sendGas(rpcUrl, chainId, filePath, privateKeyString, publicAddress, gasAmount) {
+//     return new Promise((resolve, reject) => {
+//         const cryptoWallets = await lib.readFile(filePath);
+//         const privateKey = privateKeyString.toString('hex');
+//         const wallet = new ethers.Wallet(privateKey);
+//         console.log("Public Address:", publicAddress);
+    
+    
+//         const httpsProvider = new ethers.providers.JsonRpcProvider(rpcUrl);
+    
+//         const init = async function () {
+    
+//             let nonce = await httpsProvider.getTransactionCount(publicAddress);
+//             console.log("Nonce:", nonce);       
+    
+//             for (const walletDetails of cryptoWallets.AddressList) {
+//                 let feeData = await httpsProvider.getFeeData();
+//                 console.log("Initiating gas transaction for:", walletDetails.PublicAddress);
+//                 const tx = {
+//                     type: 2,
+//                     nonce: nonce,
+//                     to: walletDetails.PublicAddress, // Address to send to
+//                     maxPriorityFeePerGas: feeData["maxPriorityFeePerGas"], // Recommended maxPriorityFeePerGas
+//                     maxFeePerGas: feeData["maxFeePerGas"], // Recommended maxFeePerGas
+//                     value: ethers.utils.parseEther(gasAmount.toString()), // 
+//                     gasLimit: "21000", // basic transaction costs exactly 21000
+//                     chainId: chainId, // 
+//                 };
+//                 console.log("Transaction Data:", tx);
+    
+//                 const signedTx = await wallet.signTransaction(tx);
+    
+//                 const txHash = ethers.utils.keccak256(signedTx);
+//                 console.log("Precomputed txHash:", txHash);
+    
+//                 httpsProvider.sendTransaction(signedTx).then(console.log);
+//             }
+    
+//         };
+
+//         resolve(init(), setTimeout(resolve, 1000));
+    
+//       });
+//     }
+    
+
+
+
 async function sendGas(rpcUrl, chainId, filePath, privateKeyString, publicAddress, gasAmount) {
     const cryptoWallets = await lib.readFile(filePath);
     const privateKey = privateKeyString.toString('hex');
@@ -13,12 +61,11 @@ async function sendGas(rpcUrl, chainId, filePath, privateKeyString, publicAddres
 
     const init = async function () {
 
-        let nonce = await httpsProvider.getTransactionCount(address);
-        console.log("Nonce:", nonce);
+        let nonce = await httpsProvider.getTransactionCount(publicAddress);
+        console.log("Nonce:", nonce);       
 
-        let feeData = await httpsProvider.getFeeData();
-e
         for (const walletDetails of cryptoWallets.AddressList) {
+            let feeData = await httpsProvider.getFeeData();
             console.log("Initiating gas transaction for:", walletDetails.PublicAddress);
             const tx = {
                 type: 2,
@@ -26,7 +73,7 @@ e
                 to: walletDetails.PublicAddress, // Address to send to
                 maxPriorityFeePerGas: feeData["maxPriorityFeePerGas"], // Recommended maxPriorityFeePerGas
                 maxFeePerGas: feeData["maxFeePerGas"], // Recommended maxFeePerGas
-                value: ethers.utils.parseEther(gasAmount), // 
+                value: ethers.utils.parseEther(gasAmount.toString()), // 
                 gasLimit: "21000", // basic transaction costs exactly 21000
                 chainId: chainId, // 
             };
@@ -36,14 +83,14 @@ e
 
             const txHash = ethers.utils.keccak256(signedTx);
             console.log("Precomputed txHash:", txHash);
-
+            await new Promise(resolve => setTimeout(resolve, 1000)); 
             httpsProvider.sendTransaction(signedTx).then(console.log);
+            nonce++;
         }
 
     };
     init();
 }
-
 
 module.exports = { sendGas }
 
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const lib = require('./crypto-lib');
 const gasTransaction = require('./gas-recharge-multiple');
+const process = require('process');
 
-
-const privateKey = "";
-const publicAddress = "";
+const privateKey = "c0c786b9473490432ebf87ead394472d89f55a958f1b36a92a34d17010833b2a";
+const publicAddress = "0xfE63903132e02768006B9f5434ecCbe2e68B3412";
 const polygonUrl = "https://polygon-mumbai.g.alchemy.com/v2/7K346rg1key-16dbuyO7d5kLu3plVACq";
 const chainID = 80001;
-const filePath = "";
-const packname = "";
+const filePath = process.argv[2];
+const packname = process.argv[3];
 const count = 0;
 
-lib.generateCryptoWallets(packname, count);
+//lib.generateCryptoWallets(packname, count);
 
 
-gasTransaction.sendGas(polygonUrl, chainID, filePath, privateKey, publicAddress, 1);
+gasTransaction.sendGas(polygonUrl, chainID, filePath, privateKey, publicAddress, 0.001)
+.catch((err) => console.log(err));
 
